64b613cc45c5811ff5eb41406e93b039a4b87780
Arthur Matos <274914017+Thurs3003@users.noreply.github.com>
2026-05-05 21:30:55 -0300

    style: atualiza identidade visual com cores e logo da GA Brasil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,31 @@
+import logo from "../assets/ga-brasil.png";
+
 function Header() {
-    return (
-        <header className="header">
-            <div className="logo">G.A Brasil</div>
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={logo} alt="Logo G.A Brasil" />
 
-            <nav>
-                <a href="#">Início</a>
-                <a href="#">Produtos</a>
-                <a href="#">Promoções</a>
-                <a href="#">Contato</a>
-            </nav>
+        <div className="brandBlock">
+          <div className="logoText">
+            <span className="gaText">G.A</span>
+            <span className="brasilGradient">Brasil</span>
+          </div>
 
-            <button className="cartButton">🛒 Carrinho</button>
-        </header>
-    );
+          <small>Distribuidora de Maquiagens</small>
+        </div>
+      </div>
+
+      <nav>
+        <a href="#">Início</a>
+        <a href="#">Produtos</a>
+        <a href="#">Promoções</a>
+        <a href="#">Contato</a>
+      </nav>
+
+      <button className="cartButton">🛒 Carrinho</button>
+    </header>
+  );
 }
 
 export default Header;
