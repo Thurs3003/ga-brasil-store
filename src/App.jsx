1e3ec5c63e33d44c22f2a0012dd4896820a31bc0
Arthur Matos <274914017+Thurs3003@users.noreply.github.com>
2026-05-06 19:47:10 -0300

    feat: adiciona persistência do carrinho no localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
-import {Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 import "./App.css";
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-  const[isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState(() => {
+    const savedCart = localStorage.getItem("@ga-brasil:cart");
+
+    if (savedCart) {
+      return JSON.parse(savedCart);
+    }
+
+    return [];
+  });
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("@ga-brasil:cart", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   function addToCart(product) {
     const existingProduct = cartItems.find((item) => item.id === product.id);
@@ -29,17 +41,20 @@ function App() {
   function increaseQuantity(productId) {
     setCartItems(
       cartItems.map((item) =>
-        item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
-      )
+        item.id === productId ? { ...item, quantity: item.quantity + 1 } : item,
+      ),
     );
   }
 
   function decreaseQuantity(productId) {
     setCartItems(
-      cartItems.map((item) =>
-        item.id === productId ? { ...item, quantity: item.quantity - 1 } : item
-      )
-      .filter((item) => item.quantity > 0)
+      cartItems
+        .map((item) =>
+          item.id === productId
+            ? { ...item, quantity: item.quantity - 1 }
+            : item,
+        )
+        .filter((item) => item.quantity > 0),
     );
   }
 
